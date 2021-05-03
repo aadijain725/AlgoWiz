0748bc565026ee4ede76bf30b593e0a455716cc8
Este Seyfried <estevanseyfried@gmail.com>
2021-05-03 11:42:47 -0700

    Basic React Routing (#10)
    * quick and dirty routing
    
    * updated react dependencies
    
    * fixed exact path naming

--- algowiz-app/src/App.js
+++ algowiz-app/src/App.js
@@ -1,25 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from 'react-router-dom';
+
+import Navbar from './components/NavBar';
+import HomePage from './components/HomePage';
+import QuizPage from './components/QuizPage';
+import LessonPage from './components/LessonPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar/> 
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+			<Switch>
+				<Route exact path="/">
+					<HomePage/>
+				</Route>
+				<Route path="/lesson">
+					<LessonPage/>
+				</Route>
+				<Route path="/quiz">
+					<QuizPage/>
+				</Route>
+				<Route path="*">
+					<h2>Bad Route</h2>
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
