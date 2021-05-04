bf58a80bbd355d1c1db290046f10fb7faae79876
Este Seyfried <estevanseyfried@gmail.com>
2021-05-04 11:00:26 -0700

    quick test fix (#20)

--- algowiz-app/src/components/NavBar.js
+++ algowiz-app/src/components/NavBar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export class NavBar extends React.Component {
     render() {
         return(
-            <div>
+            <div id="nav">
                 <h2>This is the Navbar</h2>
                 <Link to="/">Home</Link>
                 <Link to="/quiz">Quiz</Link>
