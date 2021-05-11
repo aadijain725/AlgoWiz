ec5e6833e63bea165611eb413fae0db78ae24196
Wendy Jiang <73875910+wj01011@users.noreply.github.com>
2021-05-11 12:57:47 -0700

    lesson package done (#34)
    * lesson package done
    
    * update home/lesson/quiz

--- api/algorithms/src/main/java/com/example/algorithms/quiz/QuizService.java
+++ api/algorithms/src/main/java/com/example/algorithms/quiz/QuizService.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class QuizService {
 
     private final QuizRepository quizRepository;
+
     @Autowired
     public QuizService(QuizRepository quizRepository) {
         this.quizRepository = quizRepository;
