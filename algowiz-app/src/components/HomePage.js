4841903009b9455d033a08633fede2b5012c9d53
Estevan Seyfried <estevanseyfried@gmail.com>
2021-05-17 15:39:30 -0700

    Creating API wrapper for fetch requests

--- algowiz-app/src/components/HomePage.js
+++ algowiz-app/src/components/HomePage.js
@@ -1,45 +1,31 @@
-import React from "react";
-import HomePageTopicsTabs from "./HomePageTopicsTabs";
-import { HomePageIntroduction } from "./HomePageIntroduction";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import HomePageTopicsTabs from './HomePageTopicsTabs';
+import { HomePageIntroduction } from './HomePageIntroduction';
+import { Container, Row, Col } from 'react-bootstrap';
+
+// Import the API helper class
+import APIHelper from './helpers/APIHelper';
 
 export class HomePage extends React.Component {
-    // Older style React constructions
+    // Older style React constructor
     constructor(props) {
         super(props);
         this.state = {
             homeData: null
         }
-        this.getData = this.getData.bind(this);
     }
 
     componentDidMount() {
-        this.getData();
-    }
-
-    // Fetch and parse homepage data into state
-    getData() {
-        // for browser router use this instead
-        fetch(`http://localhost:8080/api/v1/home`, {
-            credentials:'include',
-            method: 'GET',
-            headers : { 
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-            }
-        })
-        .then(response => {
-            // console.log(response);
-            return response.json()
-        })
+        // Set state from backend using APIHelper function
+        APIHelper('home')
         .then(homeData => {
-            // Save data to state
-            this.setState({homeData});
+            this.setState({homeData})
         })
         .catch(err => {
             console.log(err);
         })
     }
+
     render() {
         return (
             <Container fluid>
---
+++ algowiz-app/src/components/helpers/APIHelper.js
@@ -0,0 +1,42 @@
+/**
+ * Makes an Async fetch request to the API, must provide a path for the fetch
+ * ie: Do not supply path = 'http://localhost:8080/algowiz/quiz/quizID'
+ * instead use : path = 'quiz/quizID' 
+ * Expects to receive and return json data, may misbehave if other data returned
+ * use promise syntax in conjunction with this method. Use catch to handle errors
+ * Example usage: 
+ * APIHelper('quiz/quizID')
+ * .then(result => {
+ * 		// code for result
+ * })
+ * .catch(err => {
+ * 		// code for error handling
+ * })
+ * @param {string} path, path to fetch from API
+ * @param {string} method, optional, defaults to GET, (POST... etc)
+ */
+function APIHelper(path, method = 'GET') {
+	// check .env for which host to use (local vs production)
+	// TODO: Sanitize path input?
+	const url = `${process.env.REACT_APP_API_URL}/${path}`;
+	// return a promise
+	return fetch(url, {
+		credentials:'include',
+		method,
+		headers : { 
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		}
+	})
+	.then(response => response.json())
+	// return either the result or an error
+	.then(result => {
+		// pass along the json result
+		return result;
+	}, (error) => {
+		// Pass along the error
+		return Promise.reject(error);
+	});
+}
+
+export default APIHelper;
