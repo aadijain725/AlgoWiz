eceecfa1554beabd3376aede5bcd0a734321d685
Estevan Seyfried <estevanseyfried@gmail.com>
2021-05-21 12:36:42 -0700

    stashing Changes

--- algowiz-app/src/components/viz/VizSelectionSort.js
+++ algowiz-app/src/components/viz/VizSelectionSort.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion, AnimateSharedLayout } from "framer-motion";
 import { Col } from 'react-bootstrap';
-import VizArray from './VizArray';
 import VizRect from './VizRect';
 
 export class VizSelectionSort extends React.Component {
@@ -9,15 +8,20 @@ export class VizSelectionSort extends React.Component {
 		super(props);
 		this.state = {
 			default: [3, 2, 5, 1, 4],
-			current: [3, 2, 5, 1, 4]
+			current: [3, 2, 5, 1, 4],
+			i: 0,
+			j: 0
 		}
 	}
 
 	reset = () => {
 		console.log('pressed reset');
 		// TODO: shuffle contents instead?
+		// TODO: reset messes up default?
 		this.setState({
-			current: this.state.default
+			current: this.state.default,
+			i: 0,
+			j: 0
 		})
 	}
 
@@ -31,9 +35,31 @@ export class VizSelectionSort extends React.Component {
 
 	step = () => {
 		console.log('pressed step');
+		let i = this.state.i;
+		let j = this.state.j;
+		let min = 0;
+		let arr = this.state.current;
+  
+    // One by one move boundary of unsorted subarray 
+    if (i < arr.length - 1) { 
+        // Find the minimum element in unsorted array 
+        min = i; 
+        for (j = i + 1; j < arr.length; j++) {
+        	if (arr[j] < arr[min]) {
+            min = j; 
+					}
+				}
+        // Swap the found minimum element with the first element 
+				let first = arr[i];
+				arr[i] = arr[min];
+				arr[min] = first;
+				i++;
+    } 
 		// move around the array
 		this.setState({
-			current: [1,2,3,4,5]
+			i,
+			j,
+			current: arr
 		})
 	}
 
